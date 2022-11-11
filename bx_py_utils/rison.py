7562e68a8fface14b8347476ce5bf4aeb8d01bad
Philipp Hagemeister <philipp.hagemeister@boxine.de>
2022-11-11 14:23:56 +0100

    RISON: less quoting
    Incredibly, `now-7d` does NOT to be quoted in RISON. Add this to make nicer URLs.

--- bx_py_utils/rison.py
+++ bx_py_utils/rison.py
@@ -14,7 +14,7 @@ def rison_dumps(obj):
         return '!n'
 
     if isinstance(obj, str):
-        if re.match(r'^[a-zA-Z_][a-zA-Z0-9_]+$', obj):
+        if re.match(r'^[a-zA-Z_][-a-zA-Z0-9_]+$', obj):
             return obj  # no quoting necessary!
 
         return "'" + re.sub(r"([!'])", r'!\1', obj) + "'"
