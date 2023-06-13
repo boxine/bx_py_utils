a02632eb3af76f2c72f6cf9e1dc94f94c2ebf4ec
Philipp Hagemeister <philipp.hagemeister@boxine.de>
2023-06-13 04:21:35 +0200

    Follow RISON spec more closely
    Allow dots, and sort keys, as demanded by the spec.

--- bx_py_utils_tests/tests/test_rison.py
+++ bx_py_utils_tests/tests/test_rison.py
@@ -12,9 +12,14 @@ class RISONTest(TestCase):
         self.assertEqual(rison_dumps(''), "''")
         self.assertEqual(rison_dumps('ab'), 'ab')  # no quoting necessary!
         self.assertEqual(rison_dumps('now-2d'), 'now-2d')  # no quoting here either!
+        self.assertEqual(rison_dumps('.dot.'), '.dot.')  # even dots are fine
+        self.assertEqual(rison_dumps('1.1'), "'1.1'")  # need to be escaped when including a number
         self.assertEqual(rison_dumps('a b'), '\'a b\'')
         self.assertEqual(
             rison_dumps('a\'b\\c"d!e!!f'), "'a!'b\\c\"d!!e!!!!f'"
         )  # only ' and ! need to be escaped
         self.assertEqual(rison_dumps([]), '!()')
         self.assertEqual(rison_dumps({'x': 1, 'y z': [2, 3]}), "('x':1,'y z':!(2,3))")
+
+        # objects must be sorted
+        self.assertEqual(rison_dumps({'ab': '2nd', 'ac': 'third', 'aa': 'first'}), "(aa:first,ab:'2nd',ac:third)")
