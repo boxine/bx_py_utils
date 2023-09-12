f16edc57d03954aca926a2573a49d10c35edb51b
Philipp Hagemeister <philipp.hagemeister@boxine.de>
2023-09-12 15:32:39 +0200

    fix publish command
    Use standard library Python rather than `cli_base` (I can't even find the package).

--- bx_py_utils_tests/publish.py
+++ bx_py_utils_tests/publish.py
@@ -3,8 +3,8 @@
 """
 
 from pathlib import Path
+import subprocess
 
-from cli_base.cli_tools.subprocess_utils import verbose_check_call
 from manageprojects.utilities.publish import publish_package
 
 import bx_py_utils
@@ -19,7 +19,7 @@ def publish():
     Call this via:
         $ make publish
     """
-    verbose_check_call('make', 'test')  # don't publish if tests fail
-    verbose_check_call('make', 'fix-code-style')  # don't publish if code style wrong
+    subprocess.check_call(['make', 'test'])  # don't publish if tests fail
+    subprocess.check_call(['make', 'fix-code-style'])  # don't publish if code style wrong
 
     publish_package(module=bx_py_utils, package_path=PACKAGE_ROOT)
