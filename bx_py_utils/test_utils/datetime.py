5a3d3d458138299f7feba66a55542ca2c73383b3
Jens Diemer <jens.diemer@boxine.de>
2024-10-29 13:44:48 +0100

    Enhance parse_dt()
    Just use `fromisoformat` to support all ISO format variants, e.g.: datetime string with microseconds
    ;)

--- bx_py_utils/test_utils/datetime.py
+++ bx_py_utils/test_utils/datetime.py
@@ -1,4 +1,5 @@
 import datetime
+import sys
 
 
 def parse_dt(dtstr):
@@ -9,7 +10,16 @@ def parse_dt(dtstr):
     True
     >>> parse_dt('2020-01-01T00:00:00+0000')
     datetime.datetime(2020, 1, 1, 0, 0, tzinfo=datetime.timezone.utc)
+    >>> parse_dt('2020-01-02T03:04:05.678901+00:00')
+    datetime.datetime(2020, 1, 2, 3, 4, 5, 678901, tzinfo=datetime.timezone.utc)
     """
     if dtstr is None:
         return None
-    return datetime.datetime.strptime(dtstr, '%Y-%m-%dT%H:%M:%S%z')
+
+    try:
+        return datetime.datetime.fromisoformat(dtstr)
+    except ValueError:
+        if sys.version_info[:2] == (3, 10):
+            # Python 3.10 supports only formats emitted by date.isoformat() or datetime.isoformat() :(
+            return datetime.datetime.strptime(dtstr, '%Y-%m-%dT%H:%M:%S%z')
+        raise
